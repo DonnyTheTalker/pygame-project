e084e4964795edfa9510fc7b3065335c2041a74c
Marlesss <60238634+Marlesss@users.noreply.github.com>
2021-01-07 23:18:29 +0300

    Added save function create test_level

--- source_py/designer.py
+++ source_py/designer.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import os
 from PyQt5 import uic, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QLCDNumber, QButtonGroup
 from PyQt5.QtWidgets import QCheckBox, QMainWindow
@@ -37,6 +38,7 @@ class Main(QMainWindow):
         self.tiles = dict()  # словарь surface tile по его кодовому символу
         for i, tile_image in enumerate(cut_sheets(load_image("forest_spritesheet.png"), self.CELL_SIZE, 10, 4)):
             self.tiles[self.names[i]] = tile_image
+        self.reversed_tiles = {image: key for key, image in self.tiles.items()}
         self.initUI()
         self.resize_window()
         self.timer.start(10)
@@ -61,12 +63,13 @@ class Main(QMainWindow):
         self.tile_buttons.buttons()[0].click()
 
     def init_screen(self):
-        self.screen = pygame.display.set_mode((self.grid_width * self.CELL_SIZE, self.grid_height * self.CELL_SIZE))
+        self.screen = pygame.display.set_mode(
+            ((self.grid_width + 1) * self.CELL_SIZE, self.grid_height * self.CELL_SIZE))
         self.paint()
 
     def resize_window(self):
         width, height = self.widthBox.value(), self.heightBox.value()
-        self.grid_size = self.grid_width, self.grid_height = width + 1, height
+        self.grid_size = self.grid_width, self.grid_height = width, height
         self.init_screen()
         self.delete_abroad()
         self.paint()
@@ -110,22 +113,22 @@ class Main(QMainWindow):
             self.frontground_group.draw(self.screen)
         else:
             self.layer.draw(self.screen)
-        self.screen.blit(self.tiles[self.current_tile], ((self.grid_width - 1) * self.CELL_SIZE, 0))
+        self.screen.blit(self.tiles[self.current_tile], (self.grid_width * self.CELL_SIZE, 0))
         if self.gridMode.isChecked():
             color = pygame.Color("red")
-            for i in range(1, self.grid_width):
+            for i in range(1, self.grid_width + 1):
                 pygame.draw.line(self.screen, color, (i * self.CELL_SIZE, 0),
                                  (i * self.CELL_SIZE, self.grid_height * self.CELL_SIZE), 1)
             for j in range(1, self.grid_height):
                 pygame.draw.line(self.screen, color, (0, j * self.CELL_SIZE),
-                                 ((self.grid_width - 1) * self.CELL_SIZE, j * self.CELL_SIZE), 1)
+                                 (self.grid_width * self.CELL_SIZE, j * self.CELL_SIZE), 1)
         pygame.display.flip()
 
     def add_sprite(self, pos):
         x, y = pos
         x = x // self.CELL_SIZE * self.CELL_SIZE
         y = y // self.CELL_SIZE * self.CELL_SIZE
-        if x // self.CELL_SIZE >= self.grid_width - 1:
+        if x // self.CELL_SIZE >= self.grid_width:
             return
         self.del_sprite(pos)
         Tile(self.tiles[self.current_tile], x, y, self.all_sprites, self.layer)
@@ -137,15 +140,24 @@ class Main(QMainWindow):
                 sprite.kill()
 
     def delete_abroad(self):
-        for sprite in self.layer.sprites():
-            if (sprite.rect.right > (self.grid_width - 1) * self.CELL_SIZE or
-                    sprite.rect.bottom > self.grid_height * self.CELL_SIZE or
-                    sprite.rect.left < 0 or
-                    sprite.rect.top < 0):
+        for sprite in self.all_sprites.sprites():
+            if not (0 <= sprite.rect.x <= (self.grid_width - 1) * self.CELL_SIZE and
+                    0 <= sprite.rect.y <= (self.grid_height - 1) * self.CELL_SIZE):
                 sprite.kill()
 
     def save(self):
-        print(f'Засейвил "{self.nameEdit.text()}"')
+        name = self.nameEdit.text()
+        if not name:
+            return
+        os.mkdir(f'../data/levels/{name}')
+        for layer in ["background", "tiles", "frontground"]:
+            with open(f'../data/levels/{name}/{layer}.txt', 'w', encoding='utf-8') as file:
+                grid = [["."] * self.grid_width for i in range(self.grid_height)]
+                exec(f'self.current_group = self.{layer}_group')
+                for sprite in self.current_group.sprites():
+                    x, y = sprite.rect.x // self.CELL_SIZE, sprite.rect.y // self.CELL_SIZE
+                    grid[y][x] = self.reversed_tiles[sprite.image]
+                file.write("\n".join("".join(line) for line in grid))
 
 
 if __name__ == "__main__":
